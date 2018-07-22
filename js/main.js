3346dd2872113a334231f8fae7f7d726aaf335e1
ToniCabanas <acabascal@gmail.com>
2018-07-22 19:13:14 +0200

    Add stories

--- js/main.js
+++ js/main.js
@@ -2,14 +2,24 @@ function changeClass() {
 
   let pageHeight = window.innerHeight;
   let scrollPosition = document.body.scrollTop ? document.body.scrollTop :  document.documentElement.scrollTop;
-  console.log(pageHeight);
-  console.log(scrollPosition);
   let watto=document.getElementById("strong-watto");
-  if (scrollPosition > pageHeight){
+  let gradientTop=document.getElementById("gradient-top");
+  let gradientBottom=document.getElementById("gradient-bottom");
+
+  if (scrollPosition > pageHeight*10){
+  	  	watto.classList.add("fix");
+  	gradientTop.classList.add("hidden");
+  	gradientBottom.classList.add("hidden");
+  }
+  else if (scrollPosition > pageHeight){
   	watto.classList.add("fix");
+  	gradientTop.classList.remove("hidden");
+  	gradientBottom.classList.remove("hidden");
   }else{
   	watto.classList.remove("fix"); 
+  	gradientTop.classList.add("hidden");
+  	gradientBottom.classList.add("hidden");
   }
 }
 
-window.onscroll = function() {changeClass()};
+window.onscroll = function() {changeClass();};
